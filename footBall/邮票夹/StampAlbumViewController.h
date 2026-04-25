89e42b906ea8dde755e6cb63d3aa216eff690c43
jevon <877273033@qq.com>
2026-04-25 21:05:40 +0800

    换文件夹

--- footBall/邮票夹/StampAlbumViewController.h
+++ footBall/邮票夹/StampAlbumViewController.h
@@ -6,7 +6,7 @@
 #import "QMBaseViewController.h"
 
 NS_ASSUME_NONNULL_BEGIN
-
+/// 用户可选的已认证邮票
 @interface StampAlbumViewController : QMBaseViewController
 @property (nonatomic, copy) void (^didSelected)(PNStampAlbumItem *stamp);
 @end
