8ab8688e0195300ab0eaebd453d211567a06a3ac
snake <583151161@qq.com>
2026-04-26 16:07:36 +0800

    model

--- footBall/Core/Models/MembershipModels.m
+++ footBall/Core/Models/MembershipModels.m
@@ -7,7 +7,25 @@
 
 @implementation PNMemberPlan
 + (NSDictionary<NSString *, id> *)modelCustomPropertyMapper {
-    return @{ @"planId": @[ @"id", @"planId" ] };
+    return @{
+        @"planId": @[ @"id", @"planId", @"plan_id" ],
+        @"durationDays": @[ @"durationDays", @"duration_days" ],
+        @"dailyPriceDesc": @[ @"dailyPriceDesc", @"daily_price_desc", @"dailyPrice" ],
+        @"appleProductId": @[ @"appleProductId", @"apple_product_id", @"productId", @"product_id", @"sku", @"iapProductId", @"iosProductId" ]
+    };
+}
+
+- (BOOL)modelCustomTransformFromDictionary:(NSDictionary *)dic {
+    if ([self.planId isKindOfClass:NSString.class]) {
+        self.planId = [self.planId stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+    }
+    if ([self.appleProductId isKindOfClass:NSString.class]) {
+        self.appleProductId = [self.appleProductId stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+    }
+    if ([self.status isKindOfClass:NSString.class]) {
+        self.status = [[self.status stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]] uppercaseString];
+    }
+    return YES;
 }
 @end
 
